1b328812e6bf5d36f3dac1a12e54e7be443eef2f
riverosm <riveors.matiasa@gmail.com>
2020-06-28 17:09:45 -0300

    Subo los viajes depende la temperatura (frio o calor)

--- subte_viajes_segun_temperatura.py
+++ subte_viajes_segun_temperatura.py
@@ -1,8 +1,8 @@
 import sys
 import os.path
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu
 plt.style.use('ggplot')
 
 if len(sys.argv) < 2:
@@ -31,7 +31,7 @@ if os.path.isfile(archclima) == False:
   print("No existe el archivo " + archmclima)
   quit()
 
-print("Viajes de subte según la lluvia, si llovió más de 10mm entre las 05hs y 23hs es considerado día de lluvia")
+print("Viajes de subte según la temperatura, frío, templado o calor")
 print(" - Año:\t\t" + anio)
 print(" - Días:\t" + dias)
 print(" - Línea:\t" + linea)
@@ -58,50 +58,37 @@ if linea != "TODAS":
 dataFrameClima = pd.read_csv(archclima, header=0,sep = ';', index_col=False)
 dataFrameClima['fecha']=pd.to_datetime(dataFrameClima['fecha'],infer_datetime_format=False, exact=True,dayfirst=1)
 
-# columna RRR = lluvia, cuando hay poca lluvia ponen un string en la columna que lo remplazo por 3 mm de lluvia
-dataFrameClima.loc[dataFrameClima['RRR'].str.len() > 4, 'RRR'] = 3
+# columna T = temperatura
 
-dataFrameClima[["RRR"]] = dataFrameClima[["RRR"]].apply(pd.to_numeric)
+dataFrameClima['T'] = dataFrameClima['T'].apply(pd.to_numeric)
 
 # Cuando no hay valores, remplazo los NaN por 0
-dataFrameClima=dataFrameClima.fillna(0)
+# dataFrameClima=dataFrameClima.fillna(0)
 
 dataFrameClima.index=pd.to_datetime(dataFrameClima['fecha'],dayfirst=1)
 
-# Filtro horarios de lluvia que no sean los de uso del subte
-dataFrameClima = dataFrameClima.between_time('05:00', '23:00')
+# Como hay cuatro entradas por dia, saco promedio de las temperaturas de los 4 y me quedo con una sola entrada diaria
+dataFrameMergeado = dataFrameClima.fillna(0).groupby(by=dataFrameClima['fecha'].dt.date)['T'].mean().astype(int).reset_index(name='temperatura')
 
-# Como hay cuatro entradas por dia, sumo las llovisnas de los 4 y me quedo con una sola entrada diaria
-dataFrameMergeado = dataFrameClima.fillna(0).groupby(by=dataFrameClima['fecha'].dt.date)['RRR'].sum().reset_index(name='lluvia(mm)')
 
-def agrupa_por_lluvia(valor):
-  if valor > 0 and valor < 4:
-    return 'llovizna'
-  elif valor >= 4 and valor < 15:
-    return 'lluvia leve'
-  elif valor >= 15:
-    return 'lluvia fuerte'
+def agrupa_por_temperatura(valor):
+  if valor >= 25:
+    return 'calor'
+#  if valor > 14:
+#    return 'templado'
+  if valor <= 10:
+    return 'frío'
 
-  return 'sin lluvia'
-
-# Con que haya llovido 10mm o más ya lo considero lluvia
-def agrupa_por_lluvia_v2(valor):
-  if valor >= 10:
-    return 'con lluvia'
-
-  return 'sin lluvia'
-
-dataFrameMergeado['llueve'] =dataFrameMergeado['lluvia(mm)'].map(lambda a: agrupa_por_lluvia_v2(a))
+dataFrameMergeado['temp'] =dataFrameMergeado['temperatura'].map(lambda a: agrupa_por_temperatura(a))
 
 # Sumo todos los viajes por dia y me quedo con una sola entrada por dia
-
-dataFrameSumatoriaViajes=dataFrameViajes.groupby([dataFrameViajes['fecha'].dt.date])['total'].sum().reset_index(name='viajes')
+dataFrameSumatoriaViajes=dataFrameViajes.groupby(by=dataFrameViajes['fecha'].dt.date)['total'].sum().reset_index(name='viajes')
 
 # Divido por 1000 para que no ponga 1e6
 
 dataFrameSumatoriaViajes['viajes'] = dataFrameSumatoriaViajes.viajes / 1000
 
-# Saco los findes 
+# Saco los findes
 if dias == "semana":
   dataFrameSumatoriaViajes = dataFrameSumatoriaViajes[pd.to_datetime(dataFrameSumatoriaViajes['fecha']).dt.dayofweek <= 4 ]
 
@@ -111,18 +98,17 @@ if dias == "finde":
 dataFrameSumatoriaViajes.index.name = None
 dataFrameClima.index.name = None
 
-dataFrameMergeado = dataFrameMergeado.fillna(0)
-
 dataFrameViajesClima = pd.merge(dataFrameMergeado, dataFrameSumatoriaViajes, on='fecha')
 
-dataFrameViajesClimaAgrupado = dataFrameViajesClima.groupby(by=(dataFrameViajesClima['llueve']))
-
+dataFrameViajesClimaAgrupado = dataFrameViajesClima.groupby(by=(dataFrameViajesClima['temp']))
 
 # showfliers=False es para que no muestre los valores atípicos
 dataFrameViajesClimaAgrupado.boxplot(column='viajes', showfliers=False)
 
+listaC= dataFrameViajesClimaAgrupado.get_group('calor')['viajes'].tolist()
+#listaF= dataFrameViajesClimaAgrupado.get_group('templado')['viajes'].tolist()
+listaFr= dataFrameViajesClimaAgrupado.get_group('frío')['viajes'].tolist()
+
 print(dataFrameViajesClimaAgrupado.describe())
 
-axes = plt.gca()
-#axes.set_ylim([9500,15000])
 plt.show()
